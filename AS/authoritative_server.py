d6a14d67ef4c47a0cc2e4389d35279b40285ac5c
Chong Chen <chongchen1997@gmail.com>
2022-02-14 19:00:22 +0800

    Implement the DNS register func

---
+++ AS/authoritative_server.py
@@ -0,0 +1,62 @@
+import time
+import json
+from socket import socket, AF_INET, SOCK_DGRAM
+
+AS_IP = "127.0.0.1"
+AS_PORT = 53533
+RECV_BUF = 2048
+
+
+def handle_request(request_msg):
+    request_data = json.loads(request_msg.decode())
+    if request_data.get("VALUE") is None:
+        return handle_query(request_data)
+    else:
+        return handle_register(request_data)
+
+
+def handle_register(register_data):
+    addr_type = register_data.get("TYPE")
+    hostname = register_data.get("NAME")
+    ip_addr = register_data.get("VALUE")
+    time_to_live = register_data.get("TTL")
+    expire_time = time.time() + int(time_to_live)
+
+    # TODO: record the register info into file
+
+    response_data = {
+        "TYPE": addr_type,
+        "NAME": hostname,
+        "VALUE": ip_addr,
+        "TTL": int(expire_time - time.time())
+    }
+    return response_data
+
+
+def handle_query(query_data):
+    addr_type = query_data.get("TYPE")
+    hostname = query_data.get("NAME")
+    # TODO: Search for local record
+    ip_addr = "0.0.0.0"
+    expire_time = time.time()
+    response_data = {
+        "TYPE": addr_type,
+        "NAME": hostname,
+        "VALUE": ip_addr,
+        "TTL": int(expire_time - time.time())
+    }
+    return response_data
+
+
+def run(ip, port):
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind((ip, port))
+    while True:
+        request_msg, client_addr = sock.recvfrom(RECV_BUF)
+        response_data = handle_request(request_msg)
+        response_msg = json.dumps(response_data).encode()
+        sock.sendto(response_msg, client_addr)
+
+
+if __name__ == '__main__':
+    run(AS_IP, AS_PORT)
--- FS/fibonacci_server.py
+++ FS/fibonacci_server.py
@@ -1,15 +1,29 @@
+import json
+
 from flask import Flask, request, make_response, abort
+import socket
 
 application = Flask(__name__, static_url_path='/static')
 
+DEFAULT_DNS_TTL = 10
+RECV_BUF = 2048
 
-def parse_request_arg(args):
+
+def parse_fibonacci_arg(args):
     try:
         return int(args.get('number'))
     except TypeError:
         abort(400, 'no arg named \"number\"')
     except ValueError:
         abort(400, '\"number\" is not an int')
+
+
+def parse_register_data(register_data):
+    hostname = register_data.get("hostname")
+    ip = register_data.get("ip")
+    as_ip = register_data.get("as_ip")
+    as_port = int(register_data.get("as_port"))
+    register_data["as_port"] = as_port
 
 
 @application.route('/', methods=['GET'])
@@ -19,11 +33,33 @@ def homepage():
 
 @application.route('/fibonacci', methods=['GET'])
 def fibonacci():
-    x = parse_request_arg(request.args)
+    x = parse_fibonacci_arg(request.args)
     a, b = 0, 1
     for _ in range(x):
         a, b = b, a + b
     response = make_response(str(a), 200)
+    response.mimetype = "text/plain"
+    return response
+
+
+@application.route('/register', methods=['PUT'])
+def register():
+    register_data = request.get_json()
+    parse_register_data(register_data)
+
+    # TODO: register in the Authoritative Server via UDP over port 53533
+    as_addr = (register_data.get("as_ip"), register_data.get("as_port"))
+    dns_data = {
+        "TYPE": "A",
+        "NAME": register_data.get("hostname"),
+        "VALUE": register_data.get("ip"),
+        "TTL": DEFAULT_DNS_TTL
+    }
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(json.dumps(dns_data).encode(), as_addr)
+    as_response, _ = sock.recvfrom(RECV_BUF)
+
+    response = make_response(json.loads(as_response.decode()), 200)
     response.mimetype = "text/plain"
     return response
 
